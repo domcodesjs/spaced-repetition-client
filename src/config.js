895f9cf6ebd710206165de938ff5caa6a730d943
Dominic Faulring <domcodesjs@gmail.com>
2021-01-03 17:46:29 -0800

    fixed api uri

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_ENDPOINT: 'https://spaced-repetition-server-v1.herokuapp.com',
+  API_ENDPOINT: 'https://spaced-repetition-server-v1.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token'
 };
 
